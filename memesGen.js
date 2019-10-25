16322b23e24f5641dfc1e2e05416ac262311a300
Nicklas Bring Nielsen <nicklas.bring.nielsen@gmail.com>
2019-10-25 23:44:12 +0200

    I can check individual memes from the list

--- memesGen.js
+++ memesGen.js
@@ -1,10 +1,14 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 http.createServer(function (req, res) {
 
 if(req.url != '/favicon.ico'){
+    var q = url.parse(req.url, true);
+    console.log(q);
 
+    if(req.url == '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     var files = fs.readdirSync('./meme/'); 
     var string = "";
@@ -12,6 +16,19 @@ if(req.url != '/favicon.ico'){
         string += "<a href='.?meme=" + file + "'>" + file + "</a>" + "<br>";
     });
     res.end(string);
+    }else if(q.search != null){
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        fs.readFile('meme/' + q.query.meme, (err, image)=>{
+            res.end(image)
+        });
+    }
+    
+    else if(req.url != '/') {
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        fs.readFile(req.url.substring(1), (err, image)=>{
+            res.end(image)
+    });
+}
 
 }
 
